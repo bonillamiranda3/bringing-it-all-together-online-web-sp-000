9eb29f1cd465150b2e07f36084a179efe966d0d4
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-28 18:29:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -44,7 +44,7 @@ class Dog
 
       DB[:conn].execute(sql, name, breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs").flatten[0]
-
+      self
     end
 
     def self.create(name:, breed:)
